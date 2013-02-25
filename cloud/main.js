5d938663463af5c8d222901ec0707cb7b6ca5808
briangallagher <briangal@gmail.com>
2013-02-25 16:45:46 +0000

    addingi initial

---
+++ cloud/main.js
@@ -0,0 +1,24 @@
+var util = require('util');
+/* main.js
+ * All calls here are publicly exposed as REST API endpoints.
+ * - all parameters must be passed in a single JSON paramater.
+ * - the return 'callback' method signature is 'callback (error, data)', where 'data' is a JSON object.
+*/
+
+exports.storeInfo = function(params, callback) {
+  $fh.db({
+  "act": "create",
+  "type": "myFirstEntity",
+  "fields": {
+    "name": params.name,
+    "work": params.work
+  }
+}, function(err, data) {
+  if (err) {
+    console.log("Error " + err)
+  } else {
+    console.log(JSON.stringify(data))
+  }
+  return callback(null, {data: data});
+});
+}
