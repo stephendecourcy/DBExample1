821473085ab785eec05f2bc581d2961a4fb2b0db
stephen.decourcy@gmail.com <stephen.decourcy@gmail.com>
2013-02-26 15:00:44 +0000

    update file /client/default/js/init.js

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -29,5 +29,25 @@ $fh.ready(function() {
       }
     );
   };
-  
+   document.getElementById('report_button').onclick = function() {
+    // Invoke a cloud action call to get the remote configuration
+    // See: http://docs.feedhenry.com/wiki/Actions
+    $fh.act(
+      {
+        act:'storeInfo',
+        req: {
+          name : 'Eoin',
+          work : 'Feedhenry'
+        }
+      },
+      function(res) {
+        var name = res.data.fields.name;
+        var work = res.data.fields.work;
+        document.getElementById('cloudConfig').innerHTML = "<p>Name: " + name + "<br/>Work: "+work+"</p>";
+      },
+      function(code,errorprops,params) {
+        alert('An error occured: ' + code + ' : ' + errorprops);
+      }
+    );
+  };
 });
